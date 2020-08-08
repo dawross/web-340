a7873afc4d6f351ecb03d1dcea4d90612f801f08
Dan Ross <dawross@my365.bellevue.edu>
2020-08-08 17:23:19 -0500

    commit

---
+++ week-1/ross-assignment-1.5.js
@@ -0,0 +1,20 @@
+/*
+============================================
+; Title: Assignment 1.5
+; Author: Dan Ross
+; Date: 8 August 2020
+; Description: Node.js server
+;===========================================
+*/
+var http = require("http");
+function processRequest(req, res) {
+  var body = "Hello Universe";
+  var contentLength = body.length;
+  res.writeHead(200, {
+    "Content-Length": contentLength,
+    "Content-Type": "text/plain",
+  });
+  res.end(body);
+}
+var s = http.createServer(processRequest);
+s.listen(8080);
