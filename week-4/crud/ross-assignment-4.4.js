268036017122c7dabc256c066810826d3b76a315
Dan Ross <dawross@my365.bellevue.edu>
2020-08-31 20:43:54 -0500

    Assignment 4.4

---
+++ week-4/crud/ross-assignment-4.4.js
@@ -0,0 +1,40 @@
+/*
+============================================
+; Title: Assignment 4.4
+; Author: Dan Ross
+; Date: 31 August 2020
+; Description: cUrl
+;===========================================
+*/
+
+//Imports the ross-header.js file from the root directory
+const header = require("../../ross-header.js");
+
+// output header
+console.log(header.display("Dan", "Ross", "Exercise 3.2"));
+console.log("");
+
+var express = require("express");
+var http = require("http");
+
+var app = express();
+
+app.get("/", function (request, response) {
+  response.send("API invoked as an HTTP GET request.");
+});
+
+app.put("/", function (request, response) {
+  response.send("API invoked as an HTTP PUT request.");
+});
+
+app.post("/", function (request, response) {
+  response.send("API invoked as an HTTP POST request");
+});
+
+app.delete("/", function (request, response) {
+  response.send("API invoked as an HTTP DELETE request");
+});
+
+http.createServer(app).listen(8080, function () {
+  console.log("Application started on port 8080!");
+});
