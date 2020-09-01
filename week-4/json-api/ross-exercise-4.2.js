ea8492afad3472fe8a55f81db16f1ecef36a34ff
Dan Ross <dawross@my365.bellevue.edu>
2020-08-31 19:38:24 -0500

    commit

--- week-4/json-api/ross-exercise-4.2.js
+++ week-4/json-api/ross-exercise-4.2.js
@@ -1,40 +1,29 @@
 /*
 ============================================
-; Title: Exercise 4.3
+; Title: Exercise 4.2
 ; Author: Dan Ross
 ; Date: 31 August 2020
-; Description: HTTP Status Codes
+; Description: JSON APIs
 ;===========================================
 */
+
 //Imports the ross-header.js file from the root directory
 const header = require("../../ross-header.js");
 
 // output header
 console.log(header.display("Dan", "Ross", "Exercise 3.2"));
 console.log("");
-
 var express = require("express");
 var http = require("http");
-const { response } = require("express");
 var app = express();
-app.get("/not-found", function (req, res) {
-  res.status(404);
-  res.json({
-    error: "Resource not found.",
-  });
-});
-app.get("/ok", function (req, res) {
-  res.status(200);
-  res.json({
-    message: "OK",
-  });
-});
-app.get("/not-implemented", function (req, res) {
-  res.status(501);
-  res.json({
-    error: "Not implemented.",
+app.get("/employee/:id", function (request, response) {
+  var id = parseInt(request.params.id, 10);
+  response.json({
+    firstName: "Dan",
+    lastName: "Ross",
+    employeeId: id,
   });
 });
 http.createServer(app).listen(8080, function () {
-  console.log("Application started on port 8080!");
+  console.log("Application started on port 8080");
 });
