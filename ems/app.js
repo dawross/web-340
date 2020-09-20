ad40c780cab4d72053fd1ae2f72e62fc71cdd2b1
Dan Ross <dawross@my365.bellevue.edu>
2020-09-19 22:58:31 -0500

    ems

--- ems/app.js
+++ ems/app.js
@@ -18,8 +18,22 @@ var express = require("express");
 var http = require("http");
 var path = require("path");
 var logger = require("morgan");
-
 var app = express();
+var mongoose = require("mongoose");
+var Employee = require('./models/employee');
+// mLab connection
+var mongoDB = "mongodb+srv://admin:6553rush@buwebdev-cluster-1.cnaxx.mongodb.net/test";
+mongoose.connect(mongoDB, {
+    useMongoClient: true
+});
+
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error: "));
+db.once("open", function(){
+    console.log("Application connected to mLab MongoDB instance");
+});
+
 
 app.set("views", path.resolve(__dirname, "views"));
 app.use(express.static(__dirname + '/public'));
@@ -31,6 +45,14 @@ app.get("/", function(req, res){
         title: "Home Page"
     });
 });
+
+//model
+var employee  = new Employee({
+    firstName: "Dan",
+    lastName: "Ross"
+})
+
+
 http.createServer(app).listen(8080, function(){
     console.log("Application started on port 8080!")
 });
