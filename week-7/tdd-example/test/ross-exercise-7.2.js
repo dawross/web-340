47c6206f3dd2300055e10cce32c4ea54c4972fc6
Dan Ross <dawross@my365.bellevue.edu>
2020-09-19 21:30:15 -0500

    exercise 7.2

---
+++ week-7/tdd-example/test/ross-exercise-7.2.js
@@ -0,0 +1,22 @@
+/*
+============================================
+; Title: Exercise 7.2
+; Author: Dan Ross
+; Date: 19 September 2020
+; Description: Test Driven Development
+;===========================================
+*/
+
+//Imports the ross-header.js file from the root directory
+const header = require("../../ross-header.js");
+
+// output header
+console.log(header.display("Dan", "Ross", "Exercise 7.2"));
+console.log("");
+
+var assert = require("assert");
+describe("String#split", function(){
+    it("should return and array of fruits", function(){
+        assert(Array.isArray('Apple,Orange,Mango'.split(',')));
+    });
+});
