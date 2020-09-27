e51e23f1e520713be7ed88e84b60433f0af12248
Dan Ross <dawross@my365.bellevue.edu>
2020-09-26 19:49:18 -0500

    Ex. 8.3

--- ems/app.js
+++ ems/app.js
@@ -20,23 +20,28 @@ var path = require("path");
 var logger = require("morgan");
 var helmet = require("helmet");
 var mongoose = require("mongoose");
-var Employee = require('./models/employee');
+var Employee = require("./models/employee");
+var csrf = require("csurf");
+var cookieParser = require("cookie-parser");
+var bodyParser = require("body-parser");
+
+// setup csrf protection
+var csrfProtection = csrf({ cookie: true });
 
 //initialize express
 var app = express();
 
-
 // mLab connection
 var mongoDB = "mongodb+srv://admin:6553rush@buwebdev-cluster-1.cnaxx.mongodb.net/test";
 mongoose.connect(mongoDB, {
-    useMongoClient: true
+  useMongoClient: true,
 });
 
 mongoose.Promise = global.Promise;
 var db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error: "));
-db.once("open", function(){
-    console.log("Application connected to mLab MongoDB instance");
+db.once("open", function () {
+  console.log("Application connected to mLab MongoDB instance");
 });
 
 //set statements
@@ -44,24 +49,48 @@ app.set("views", path.resolve(__dirname, "views"));
 app.set("view engine", "ejs");
 
 //use statements
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 app.use(logger("short"));
 app.use(helmet.xssFilter());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.use(cookieParser());
+app.use(csrfProtection);
+app.use(function (request, response, next) {
+  var token = request.csrfToken();
+  response.cookie("XSRF-TOKEN", token);
+  response.locals.csrfToken = token;
+  next();
+});
+
 // http calls
-app.get("/", function(req, res){
-    res.render("index", {
-        title: "Home Page"
-    });
+app.get("/", function (req, res) {
+  res.render("index", {
+    title: "Home Page",
+  });
+});
+
+app.get("/new", function (req, res) {
+  res.render("new", {
+    title: "Add New Employee",
+  });
+});
+
+app.post("/process", function (request, response) {
+  console.log(request.body.txtName);
+  response.redirect("/");
 });
 
 //model
-var employee  = new Employee({
-    firstName: "Dan",
-    lastName: "Ross"
-})
-
+var employee = new Employee({
+  firstName: "Dan",
+  lastName: "Ross",
+});
 
 // create/start Node server
-http.createServer(app).listen(8080, function(){
-    console.log("Application started on port 8080!")
+http.createServer(app).listen(8080, function () {
+  console.log("Application started on port 8080!");
 });
